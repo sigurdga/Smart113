6636654b32d2b11a650881b7986fd4a77049604d
Sigurd Gartmann <sigurdga@sigurdga.no>
2012-08-04 15:39:06 +0200

    Reset migrations

--- smart113/core/migrations/0001_initial.py
+++ smart113/core/migrations/0001_initial.py
@@ -8,6 +8,16 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'Relationship'
+        db.create_table('core_relationship', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('from_person', self.gf('django.db.models.fields.related.ForeignKey')(related_name='from_people', to=orm['core.UserProfile'])),
+            ('to_person', self.gf('django.db.models.fields.related.ForeignKey')(related_name='to_people', to=orm['core.UserProfile'])),
+            ('relation', self.gf('django.db.models.fields.IntegerField')()),
+            ('same_address', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('core', ['Relationship'])
+
         # Adding model 'Language'
         db.create_table('core_language', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -16,28 +26,71 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('core', ['Language'])
 
+        # Adding model 'Phone'
+        db.create_table('core_phone', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('number', self.gf('django.db.models.fields.CharField')(max_length=20)),
+        ))
+        db.send_create_signal('core', ['Phone'])
+
         # Adding model 'UserProfile'
         db.create_table('core_userprofile', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('primary_language', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Language'], null=True, blank=True)),
+            ('birth_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
             ('gender', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
-            ('primary_language', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Language'], null=True)),
-            ('hair_color', self.gf('django.db.models.fields.CharField')(max_length=20, null=True, blank=True)),
-            ('eye_color', self.gf('django.db.models.fields.CharField')(max_length=20, null=True, blank=True)),
-            ('weight', self.gf('django.db.models.fields.SmallIntegerField')(null=True)),
-            ('height', self.gf('django.db.models.fields.SmallIntegerField')(null=True)),
-            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True)),
+            ('street_address', self.gf('django.db.models.fields.CharField')(max_length=60, blank=True)),
+            ('postcode', self.gf('django.db.models.fields.CharField')(max_length=10, blank=True)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('hair_color', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
+            ('eye_color', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
+            ('weight', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('height', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
             ('extra', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('sight', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('hearing', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('speaking', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('mobility', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('allergies', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('amputation', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('blind', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('cognitive', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('stationary', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('wheelchair', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('deaf', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('mute', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('life_support', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('walker', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('oxygen', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
         ))
         db.send_create_signal('core', ['UserProfile'])
 
+        # Adding M2M table for field phones on 'UserProfile'
+        db.create_table('core_userprofile_phones', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('userprofile', models.ForeignKey(orm['core.userprofile'], null=False)),
+            ('phone', models.ForeignKey(orm['core.phone'], null=False))
+        ))
+        db.create_unique('core_userprofile_phones', ['userprofile_id', 'phone_id'])
+
 
     def backwards(self, orm):
+        # Deleting model 'Relationship'
+        db.delete_table('core_relationship')
+
         # Deleting model 'Language'
         db.delete_table('core_language')
 
+        # Deleting model 'Phone'
+        db.delete_table('core_phone')
+
         # Deleting model 'UserProfile'
         db.delete_table('core_userprofile')
+
+        # Removing M2M table for field phones on 'UserProfile'
+        db.delete_table('core_userprofile_phones')
 
 
     models = {
@@ -83,18 +136,52 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
+        'core.phone': {
+            'Meta': {'object_name': 'Phone'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'core.relationship': {
+            'Meta': {'object_name': 'Relationship'},
+            'from_person': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'from_people'", 'to': "orm['core.UserProfile']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'relation': ('django.db.models.fields.IntegerField', [], {}),
+            'same_address': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'to_person': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'to_people'", 'to': "orm['core.UserProfile']"})
+        },
         'core.userprofile': {
             'Meta': {'object_name': 'UserProfile'},
+            'allergies': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'amputation': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'blind': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'cognitive': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'deaf': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
             'extra': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'eye_color': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'eye_color': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
             'gender': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'hair_color': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
-            'height': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True'}),
+            'hair_color': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'hearing': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'height': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
-            'primary_language': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Language']", 'null': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'life_support': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'mobility': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'mute': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'oxygen': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'phones': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Phone']", 'symmetrical': 'False', 'blank': 'True'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'primary_language': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Language']", 'null': 'True', 'blank': 'True'}),
+            'relationships': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'related_to'", 'symmetrical': 'False', 'through': "orm['core.Relationship']", 'to': "orm['core.UserProfile']"}),
+            'sight': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'speaking': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'stationary': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'street_address': ('django.db.models.fields.CharField', [], {'max_length': '60', 'blank': 'True'}),
             'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'}),
-            'weight': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True'})
+            'walker': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'wheelchair': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'})
         }
     }
 
